27f8822415b2e16b94ecf179b689f0c119b84e78
bertrandsimon <trandber@hotmail.com>
2024-04-23 18:16:48 +0200

    Quicksand font installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Quicksand } from 'next/font/google'
+import Head from 'next/head';
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
+const quicksand = Quicksand({
+  subsets: ['latin'],
+})
 export const metadata: Metadata = {
   title: "Pliage Aluminium et Acier",
   description: "à remplir",
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr">
+    
+      <body className={quicksand.className}>{children}</body>
+
     </html>
   );
 }
