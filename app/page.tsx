52ba3a0c41cec95a3585fa328e64ae57afeabbda
bertrandsimon <trandber@hotmail.com>
2024-05-10 12:55:44 +0200

    Presentation grid structure done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import Banner from "@/components/homepage/banner";
 import Steps from "@/components/homepage/steps";
+import Presentation from "@/components/homepage/presentation";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-10">
-       <Banner />
+    <main className="flex min-h-screen flex-col items-center gap-14">
+
+      <Banner />
       <Steps />
+      <Presentation />
+
     </main>
   );
 }
---
+++ components/homepage/presentation.tsx
@@ -0,0 +1,38 @@
+
+import Link from "next/link"
+import Image from "next/image"
+
+
+export default function Presentation() {
+  return (
+  
+  <div className="grid grid-cols-2 gap-12">
+
+    <div id="col1">
+      
+      <div  id="line1" className="flex flex-col gap-2 pl-12 justify-center h-full">
+        <p className="font-bold">Lorem ipsum dolor</p>
+        <p className="font-bold text-2xl">Texte présentation</p>
+        <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis dui et ipsum tempus sollicitudin. Etiam dapibus nec nulla sit amet viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis dui et ipsum tempus sollicitudin. Etiam dapibus nec nulla sit amet viverra. </p>
+        <div className="lgBtn greyBtn max-w-[124px] my-2">en savoir +</div>
+       </div>
+
+    </div>
+
+    <div id="col2 flex flex-col pr-12 gap-2">
+
+      <div className="flex items-center justify-end gap-2 pb-4">
+        <Image src="/images/arrow-small.jpg" width={19} height={19} alt="" className="mt-4"/>
+        <p className="text-xl">Besoin</p>
+        <p className="text-xl font-bold">d'inspirations ?</p>
+      </div>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet accusamus est iste natus, modi quos? Architecto eligendi eaque rerum est nihil ducimus accusantium vero quae, nobis accusamus dolore veritatis cum.</p>
+
+    </div>
+
+  </div>
+  
+  
+  )
+}
