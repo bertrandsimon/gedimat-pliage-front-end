c7e71615cd3bd870e6a99a9e43a1decee9058e4a
bertrandsimon <trandber@hotmail.com>
2024-05-10 09:17:01 +0200

    Banner done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-
+import Banner from "@/components/homepage/banner";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-10">
-       
+       <Banner />
         
     </main>
   );
---
+++ components/homepage/banner.tsx
@@ -0,0 +1,55 @@
+
+import Link from "next/link"
+import Image from "next/image"
+// import gedimat from "@/public/images/banner/gedimat.png"
+import { CheckCircleIcon } from '@heroicons/react/20/solid'
+
+export default function Banner() {
+  return (
+  
+   <div className="w-full grid grid-cols-2 bg-[#F2EDEA] rounded-md h-[600px] gap-10">
+
+   <section id="col1" className="pl-12 py-12">
+
+    <div className="flex flex-col h-full justify-around">
+
+      <div  id="line1" className="flex flex-col gap-2">
+
+         <p className="font-bold">Spécialiste du pliage</p>
+         <p className="font-bold text-3xl">Aluminium et acier</p>
+         <p className="max-w-[420px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis dui et ipsum tempus sollicitudin. Etiam dapibus nec nulla sit amet viverra. </p>
+         <div className="lgBtn redBtn max-w-[124px] my-2">en savoir +</div>
+      
+      </div>
+
+       <div id="line2" className="flex gap-8 justify-start items-center">
+
+          <div className="rounded-md flex flex-col justify-center items-center text-center gap-4 dashed p-4">
+            <Image src="/images/banner/gedimat.png" height={58} width={121} alt=""/>
+            <span className="text-sm">Toute l’expérience du groupe <br />Gedimat à votre service</span>
+          </div>
+
+          <div className="rounded-md flex flex-col justify-center items-center text-center gap-4 dashed p-4">
+            <Image src="/images/banner/delivery.png" height={58} width={71} alt=""/>
+            <span className="text-sm">Toute l’expérience du groupe <br />Gedimat à votre service</span>
+          </div>
+
+
+      </div>
+
+    </div>
+
+   </section>
+
+
+   <section id="col2" className="pr-12 py-12 h-full bg-[url('/images/banner/slice.png')] bg-no-repeat bg-right">
+
+      <Image src="/images/banner/house.png" height={497} width={592}  alt=""/>
+
+   </section> 
+
+   </div>
+  
+  
+  )
+}
