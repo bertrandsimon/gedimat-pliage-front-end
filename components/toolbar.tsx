dbf138d957712f6c0fb7e2e601bca62f604fffbb
bertrandsimon <trandber@hotmail.com>
2024-05-19 11:06:18 +0200

    Modal and user signin compoonents created

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -16,6 +16,19 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+
 export default function Toolbar() {
   return (
   
@@ -60,6 +73,25 @@ export default function Toolbar() {
             </DropdownMenuContent>
 
           </DropdownMenu>
+
+          <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
 
         </div>
 
