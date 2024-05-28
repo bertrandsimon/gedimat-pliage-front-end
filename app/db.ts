c03e02f2f6db937a450d22e719d1336c99dc7b18
bertrandsimon <trandber@hotmail.com>
2024-05-28 16:04:34 +0200

    Mongoose bug fixed

--- app/db.ts
+++ app/db.ts
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://jobimat:O2AnB6A1jOokWVb1@cluster0.qdjfkjz.mongodb.net/pliagealuminium', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      //: true,
+      //useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    //console.log('MongoDB connected');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    //console.error('MongoDB connection error:', error);
   }
 };
 
