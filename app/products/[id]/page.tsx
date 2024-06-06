72f9af0dd4c697fe46711262921647737d19a55b
bertrandsimon <trandber@hotmail.com>
2024-06-06 11:25:18 +0200

    Test

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,22 +1,25 @@
 
 import Image from 'next/image'
 import SingleProduct from '@/app/singleProduct/singleProduct'
-
+import { NextResponse } from "next/server";
+import connectDB from "@/app/db";
+import Product from "@/app/models/Product";
+import { ObjectId } from "mongodb";
 export const dynamic = 'force-dynamic'
 
-export async function fetchSingleProduct( {params}:any ) {
+// export async function fetchSingleProduct( {params}:any ) {
    
  
-  const response = await fetch(`${process.env.URL}/api/products/${params.id}`, { cache: 'no-store' })
+//   const response = await fetch(`${process.env.URL}/api/products/${params.id}`, { cache: 'no-store' })
 
-  const item = await response.json()
+//   const item = await response.json()
 
    
     
-}
+// }
 
 
-export default function Page() {
+export default function Page({params}:any) {
    
  
   // const response = await fetch(`${process.env.URL}/api/products/${params.id}`, { cache: 'no-store' })
@@ -30,10 +33,3 @@ export default function Page() {
     )
 }
 
-// export default function productPage (){
-//   return (
-//     <div>
-//       <h1>test</h1>
-//     </div>
-//   )
-// }
