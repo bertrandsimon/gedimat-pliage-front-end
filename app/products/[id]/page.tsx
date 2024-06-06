eb5e62c293fcdb4f9f18b1870c2c4df9c3ac0c68
bertrandsimon <trandber@hotmail.com>
2024-06-06 10:31:13 +0200

    Vercel test

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,21 +1,21 @@
 
-import Image from 'next/image'
-import SingleProduct from '@/app/singleProduct/singleProduct'
+// import Image from 'next/image'
+// import SingleProduct from '@/app/singleProduct/singleProduct'
 
-//export const dynamic = 'force-dynamic'
-export async function productPage( {params}:any ) {
+// //export const dynamic = 'force-dynamic'
+// export async function productPage( {params}:any ) {
    
  
-  const response = await fetch(`${process.env.URL}/api/products/${params.id}`, { cache: 'no-store' })
+//   const response = await fetch(`${process.env.URL}/api/products/${params.id}`, { cache: 'no-store' })
 
-  const item = await response.json()
+//   const item = await response.json()
 
-    return (
+//     return (
         
-    <div>
-      <SingleProduct item={item}/>
-      <h1>test</h1>
-    </div>
-    )
-}
+//     <div>
+//       <SingleProduct item={item}/>
+//       <h1>test</h1>
+//     </div>
+//     )
+// }
 
