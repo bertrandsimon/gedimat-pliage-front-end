f9f6ad41878c7b9ca6da3cf66ce81cd1fca04df5
bertrandsimon <trandber@hotmail.com>
2024-12-12 15:47:46 +0100

    React hook form installed

--- app/contact/contact.tsx
+++ app/contact/contact.tsx
@@ -1,20 +1,26 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
+'use client'
+import { useForm } from 'react-hook-form'
+
+import {
+  BuildingOffice2Icon,
+  EnvelopeIcon,
+  PhoneIcon,
+} from '@heroicons/react/24/outline'
 
 export default function Contact() {
+  // register - permet d'enregistrer un champ de formulaire auprès de React Hook Form
+  // handleSubmit - permet de récupérer toutes les données du formulaire et de les envoyer à une méthode très spécifique
+  // errors - permet de savoir lorsqu'un champ de formulaire est invalide
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmitHandler = (data: any) => {
+    console.log(data)
+  }
+
   return (
     <div className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
@@ -41,43 +47,64 @@ export default function Contact() {
                 <svg x="100%" y={-1} className="overflow-visible fill-gray-50">
                   <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
                 </svg>
-                <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
+                <rect
+                  width="100%"
+                  height="100%"
+                  strokeWidth={0}
+                  fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
+                />
               </svg>
             </div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900">Contactez-nous</h2>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900">
+              Contactez-nous
+            </h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Notre équipe de spécialistes répond à toutes vos questions en moins de 24 h.
+              Notre équipe de spécialistes répond à toutes vos questions en
+              moins de 24 h.
             </p>
             <dl className="mt-10 space-y-4 text-base leading-7 text-gray-600">
               <div className="flex gap-x-4">
                 <dt className="flex-none">
-                 
-                  <BuildingOffice2Icon className="h-7 w-6 text-gray-400" aria-hidden="true" />
+                  <BuildingOffice2Icon
+                    className="h-7 w-6 text-gray-400"
+                    aria-hidden="true"
+                  />
                 </dt>
                 <dd>
-                9 RUE DE NORMANDI
+                  9 RUE DE NORMANDI
                   <br />
                   80220 GAMACHES
                 </dd>
               </div>
               <div className="flex gap-x-4">
                 <dt className="flex-none">
-                 
-                  <PhoneIcon className="h-7 w-6 text-gray-400" aria-hidden="true" />
+                  <PhoneIcon
+                    className="h-7 w-6 text-gray-400"
+                    aria-hidden="true"
+                  />
                 </dt>
                 <dd>
-                  <a className="hover:text-gray-900" href="tel:+1 (555) 234-5678">
-                  02 35 50 85 85
+                  <a
+                    className="hover:text-gray-900"
+                    href="tel:+1 (555) 234-5678"
+                  >
+                    02 35 50 85 85
                   </a>
                 </dd>
               </div>
               <div className="flex gap-x-4">
                 <dt className="flex-none">
                   <span className="sr-only">Email</span>
-                  <EnvelopeIcon className="h-7 w-6 text-gray-400" aria-hidden="true" />
+                  <EnvelopeIcon
+                    className="h-7 w-6 text-gray-400"
+                    aria-hidden="true"
+                  />
                 </dt>
                 <dd>
-                  <a className="hover:text-gray-900" href="mailto:hello@example.com">
+                  <a
+                    className="hover:text-gray-900"
+                    href="mailto:hello@example.com"
+                  >
                     contact@pliage-aluminium.com
                   </a>
                 </dd>
@@ -85,80 +112,93 @@ export default function Contact() {
             </dl>
           </div>
         </div>
-        <form action="#" method="POST" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+        <form
+          onSubmit={handleSubmit(onSubmitHandler)}
+          className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
+        >
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
-                <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
                   Prénom
                 </label>
                 <div className="mt-2.5">
                   <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
+                    {...register('surname', { required: true })}
+                    placeholder="Prénom"
+                    className="input block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div>
-                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
                   Nom
                 </label>
                 <div className="mt-2.5">
                   <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
+                    {...register('name', { required: true })}
+                    placeholder="Nom"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#D9D9D9] sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
                   Email
                 </label>
                 <div className="mt-2.5">
                   <input
                     type="email"
-                    name="email"
-                    id="email"
+                    {...register('email', { required: true })}
+                    placeholder="Email"
                     autoComplete="email"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#D9D9D9] sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
                   Téléphone
                 </label>
                 <div className="mt-2.5">
                   <input
-                    type="tel"
-                    name="phone-number"
-                    id="phone-number"
-                    autoComplete="tel"
+                    {...register('phone', { required: true })}
+                    type="number"
+                    placeholder="Téléphone"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#D9D9D9] sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
                   Message
                 </label>
                 <div className="mt-2.5">
                   <textarea
-                    name="message"
-                    id="message"
+                    {...register('message', { required: true })}
+                    placeholder="Message"
                     rows={4}
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#D9D9D9] sm:text-sm sm:leading-6"
-                    defaultValue={''}
                   />
                 </div>
               </div>
             </div>
+
             <div className="mt-8 flex justify-end">
               <button
                 type="submit"
