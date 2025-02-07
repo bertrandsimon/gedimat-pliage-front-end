154d2968e033b8f6ce5e829496bd9f82c484ece8
bertrandsimon <trandber@hotmail.com>
2025-02-07 11:21:10 +0100

    Page presentation done

--- app/presentation/page.tsx
+++ app/presentation/page.tsx
@@ -3,6 +3,7 @@ import Ariane from '@/components/ariane'
 import Stats from '@/components/presentation/stats'
 import Presentation from '@/components/homepage/presentation'
 import Top from '@/components/presentation/top'
+import ImagesArea from '@/components/presentation/imagesArea'
 
 export default function PresentationPage() {
   const ariane = {
@@ -14,28 +15,35 @@ export default function PresentationPage() {
   return (
     <div>
       <Ariane ariane={ariane} />
-      <div className="px-2 py-4">
+      {/* <div className="px-2 py-4">
         <Presentation />
-      </div>
-      <div className="px-4 py-12">
+      </div> */}
+      <div className="px-12 py-12">
         <Top
           title="Spécialiste du pliage"
           subtitle="Aluminium et acier"
           sliderOn={true}
           imageTop="man.png"
         ></Top>
+        <div className="-mt-60">
+          <ImagesArea></ImagesArea>
+        </div>
+
         <Stats />
-        <div className="flex flex-col gap-4">
+        <div className="grid grid-cols-3 py-16 px-12 gap-12">
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
             tenetur, incidunt accusantium explicabo unde molestiae ea id iusto
             cum corrupti ut qui, at maxime voluptatum illo dicta magnam laborum
-            impedit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Quam tenetur, incidunt accusantium explicabo unde molestiae ea id
-            iusto cum corrupti ut qui, at maxime voluptatum illo dicta magnam
-            laborum impedit.
+            impedit.
           </p>
 
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
+            tenetur, incidunt accusantium explicabo unde molestiae ea id iusto
+            cum corrupti ut qui, at maxime voluptatum illo dicta magnam laborum
+            impedit.
+          </p>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
             tenetur, incidunt accusantium explicabo unde molestiae ea id iusto
---
+++ components/presentation/imagesArea.tsx
@@ -0,0 +1,88 @@
+import Link from 'next/link'
+
+function ImagesArea() {
+  return (
+    <div>
+      <main className="isolate">
+        <div className="overflow-hidden">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+              <div
+                id="line1"
+                className="flex flex-col gap-2 pl-12 justify-center h-full mt-[300px]"
+              >
+                <p className="font-medium">Votre expert en</p>
+                <p className="font-bold text-2xl">
+                  Pliage aluminium sur mesure{' '}
+                </p>
+                <p className="text-md">
+                  Optez pour le pliage aluminium sur mesure avec Gedimat, votre
+                  expert en solutions adaptées à vos besoins. <br />
+                  <br />
+                  Que ce soit pour un projet résidentiel ou professionnel,
+                  bénéficiez d’un service précis et fiable. Nous transformons
+                  vos idées en réalisations concrètes, avec un aluminium de
+                  qualité, parfaitement ajusté à vos attentes. <br />
+                  <br />
+                  Faites confiance à notre expertise et démarquez-vous grâce à
+                  des finitions sur mesure, pensées pour durer dans le temps et
+                  répondre à vos exigences.
+                </p>
+              </div>
+              <div className="mt-24 pt-12 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                  <div className="relative">
+                    <img
+                      src="/images/medias/photos/1.jpg"
+                      alt=""
+                      className="aspect-[2/3] w-full rounded-xl object-cover "
+                    />
+                    <div className="pointer-events-none absolute inset-0 rounded-xl " />
+                  </div>
+                </div>
+                <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                  <div className="relative">
+                    <img
+                      src="/images/medias/photos/2.jpg"
+                      alt=""
+                      className="aspect-[2/3] w-full rounded-xl object-cover "
+                    />
+                    <div className="pointer-events-none absolute inset-0 rounded-xl " />
+                  </div>
+                  <div className="relative">
+                    <img
+                      src="/images/medias/photos/3.jpg"
+                      alt=""
+                      className="aspect-[2/3] w-full rounded-xl object-cover "
+                    />
+                    <div className="pointer-events-none absolute inset-0 rounded-xl " />
+                  </div>
+                </div>
+                <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+                  <div className="relative">
+                    <img
+                      src="/images/medias/photos/4.jpg"
+                      alt=""
+                      className="aspect-[2/3] w-full rounded-xl object-cover "
+                    />
+                    <div className="pointer-events-none absolute inset-0 rounded-xl " />
+                  </div>
+                  <div className="relative">
+                    <img
+                      src="/images/medias/photos/5.jpg"
+                      alt=""
+                      className="aspect-[2/3] w-full rounded-xl object-cover "
+                    />
+                    <div className="pointer-events-none absolute inset-0 rounded-xl " />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default ImagesArea
