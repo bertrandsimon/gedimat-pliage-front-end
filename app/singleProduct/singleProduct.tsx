2ce29784aab65bb1f280c3b42e6e3049ecbbe301
bertrandsimon <trandber@hotmail.com>
2025-02-19 16:18:16 +0100

    Added Angle calculation guide

--- app/singleProduct/singleProduct.tsx
+++ app/singleProduct/singleProduct.tsx
@@ -607,6 +607,41 @@ export default function SingleProduct({
                         ></Image>
                       </DialogContent>
                     </Dialog>
+                    <div className="py-6 flex justify-start items-center gap-4">
+                      <span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="size-5"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                          />
+                        </svg>
+                      </span>
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <span className="cursor-pointer text-sm underline">
+                            Guide calcul angles
+                          </span>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[600px]">
+                          <DialogTitle></DialogTitle>
+                          <Image
+                            src="/images/products/angle_calculation/calculation_angle.jpg"
+                            width={600}
+                            height={600}
+                            alt="Guide de calcul des angles"
+                            className="rounded-md"
+                          />
+                        </DialogContent>
+                      </Dialog>
+                    </div>
                   </div>
 
                   {/* col1 */}
