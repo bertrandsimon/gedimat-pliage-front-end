cca0f927368acd330fd44f0feac914ffb44e4955
logeen <trandber@hotmail.com>
2025-09-17 17:59:18 +0200

    Added links on partners

--- components/homepage/partners.tsx
+++ components/homepage/partners.tsx
@@ -15,19 +15,39 @@ export default function Partners() {
 
       <ScrollArea className="w-full flex justify-center items-center rounded-md border p-4 py-2 mb-20">
         <div className="flex w-full space-x-10 p-4 justify-around items-center">
-          <div className="grayscale hover:grayscale-0 cursor-pointer flex  ">
+          <a
+            href="https://www.prefa.fr"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grayscale hover:grayscale-0 cursor-pointer flex"
+          >
             <Image src={partner1} alt="" width={85} height={94} />
-          </div>
+          </a>
 
-          <div className="grayscale hover:grayscale-0 cursor-pointer flex">
+          <a
+            href="https://www.bacacier.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grayscale hover:grayscale-0 cursor-pointer flex"
+          >
             <Image src={partner3} alt="" width={173} height={40} />
-          </div>
-          <div className="grayscale hover:grayscale-0 cursor-pointer flex -mt-4">
+          </a>
+          <a
+            href="https://e-steel.arcelormittal.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grayscale hover:grayscale-0 cursor-pointer flex -mt-4"
+          >
             <Image src={partner4} alt="" width={176} height={41} />
-          </div>
-          <div className="grayscale hover:grayscale-0 cursor-pointer flex -mt-2">
+          </a>
+          <a
+            href="https://www.rheinzink.fr"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grayscale hover:grayscale-0 cursor-pointer flex -mt-2"
+          >
             <Image src={partner5} alt="" width={205} height={32} />
-          </div>
+          </a>
 
         </div>
         <ScrollBar orientation="horizontal" />
